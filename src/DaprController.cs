09932ee24466eb66341af3da9d81567e3cf2eb41
Per Ökvist <perokvist@users.noreply.github.com>
2020-10-27 10:21:52 +0100

    init

---
+++ src/DaprController.cs
@@ -0,0 +1,43 @@
+﻿using Dapr;
+using Dapr.Client;
+using Dapr.Client.Autogen.Grpc.v1;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace push
+{
+    [ApiController]
+    public class DaprController : ControllerBase
+    {
+        private readonly DaprClient daprClient;
+
+        public DaprController(DaprClient daprClient)
+        {
+            this.daprClient = daprClient;
+        }
+
+        [Topic("pubsub", "in")]
+        [HttpPost("in")]
+        public async Task<IActionResult> InboxAsync(ProductInfo productInfo)
+        {
+            var d = new Dictionary<string, string>
+            {
+                { "blobName", "index.html" },
+                { "ContentType", "text/html" }
+            };
+            await daprClient.InvokeBindingAsync("azurestorage", "create", Template(productInfo), metadata: d);
+            return Ok();
+        }
+
+        public static string Template(ProductInfo info)
+            => $"<html><body><h1>{info.Title}</h1><div>{info.Price}</div></body></html>";
+    }
+
+    public class ProductInfo
+    {
+        public string Title { get; set; }
+
+        public int Price { get; set; }
+    }
+}
