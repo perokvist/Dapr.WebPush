6cf90d4b1f3a650241d25474a06f1bcc2025a037
Per Ökvist <perokvist@users.noreply.github.com>
2020-11-09 17:11:36 +0100

    meta merkup

--- src/csharppush/DaprController.cs
+++ src/csharppush/DaprController.cs
@@ -42,8 +42,8 @@ namespace push
 @$"<!DOCTYPE html>
 <html lang=""en"">
 <head>
-  <meta charset = ""UTF-8"" >
-  < meta name=""viewport"" content=""width=device-width, initial-scale=1.0"">
+  <meta charset = ""UTF-8"">
+  <meta name=""viewport"" content=""width=device-width, initial-scale=1.0"">
   <title>Product</title>
   <link rel = ""stylesheet"" href=""product.css"" />
 </head>
